8848924cbc0059c59ba71b710d0a470eb5e93418
Mai Xuan Nghia <nghia.mai@sapotacorp.vn>
2025-02-11 08:19:46 +0700

    first commit

---
+++ src/app/components/Container.js
@@ -0,0 +1,14 @@
+export default function Container({ children }) {
+    return (
+      <div style={containerStyle}>
+        {children}
+      </div>
+    );
+  }
+  
+  const containerStyle = {
+    maxWidth: "1440px",
+    margin: "0 auto",
+    padding: "0 20px",
+  };
+  
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Container from "./components/Container";
+import Header from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Header></Header>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Container>{children}</Container>
       </body>
     </html>
   );
